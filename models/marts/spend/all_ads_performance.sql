153a500726c2bba1309d6ed30d1fd7cfc3056d09
Riley Sorenson <56692568+wizard7558@users.noreply.github.com>
2025-12-13 11:06:28 -0800

    Add is_optimization_conversion to all_ads_performance

--- models/marts/spend/all_ads_performance.sql
+++ models/marts/spend/all_ads_performance.sql
@@ -8,30 +8,32 @@ SELECT
     campaign,
     adset AS ad_group,
     conversionname AS conversion_name,
+    is_optimization_conversion,
     SUM(spend) AS spend,
     SUM(impressions) AS impressions,
     SUM(clicks) AS clicks,
     SUM(allconv) AS conversions,
     NULL AS conversion_value
 FROM {{ ref('facebook_ads_performance') }}
-GROUP BY 1, 2, 3, 4, 5, 6
+GROUP BY 1, 2, 3, 4, 5, 6, 7
 
 UNION ALL
 
--- Google Ads at Campaign level (use campaign_performance for accurate totals)
+-- Google Ads at Campaign level with conversion detail
 SELECT
     date,
     'Google' AS platform,
     account,
     campaign,
     NULL AS ad_group,
-    NULL AS conversion_name,
+    conversion_action_name AS conversion_name,
+    is_optimization_conversion,
     SUM(spend) AS spend,
     SUM(impressions) AS impressions,
     SUM(clicks) AS clicks,
     SUM(conversions) AS conversions,
     SUM(conversion_value) AS conversion_value
 FROM {{ ref('google_ads_campaign_performance') }}
-GROUP BY 1, 2, 3, 4
+GROUP BY 1, 2, 3, 4, 5, 6, 7
 
 ORDER BY 1 DESC
