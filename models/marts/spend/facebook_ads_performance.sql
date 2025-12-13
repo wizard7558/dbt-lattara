78bf30a3d9e422be31095b8c2d3dd00272dee06a
Riley Sorenson <56692568+wizard7558@users.noreply.github.com>
2025-12-13 10:14:07 -0800

    Add is_optimization_conversion flag to Facebook model

--- models/marts/spend/facebook_ads_performance.sql
+++ models/marts/spend/facebook_ads_performance.sql
@@ -1,17 +1,22 @@
- {{config(materialized = "table")}} 
+{{config(materialized = "table")}} 
 
-SELECT CAST(DATE AS DATE) AS DATE
-     , ACCOUNT
-     , CAMPAIGN
-     , ADSET
-     , AD
-     , AD_ID
-     , CONVERSIONNAME
-     , SPEND_RAW/DIVIDEND AS SPEND
-     , IMPRESSIONS_RAW/DIVIDEND AS IMPRESSIONS
-     , CLICKS_RAW/DIVIDEND AS CLICKS
-     , ALLCONV
-FROM {{ ref('int_facebook_conversions') }}
+SELECT CAST(f.DATE AS DATE) AS DATE
+     , f.ACCOUNT
+     , f.CAMPAIGN
+     , f.ADSET
+     , f.AD
+     , f.AD_ID
+     , f.CONVERSIONNAME
+     , f.SPEND_RAW/f.DIVIDEND AS SPEND
+     , f.IMPRESSIONS_RAW/f.DIVIDEND AS IMPRESSIONS
+     , f.CLICKS_RAW/f.DIVIDEND AS CLICKS
+     , f.ALLCONV
+     , CASE WHEN oc.optimization_conversion IS NOT NULL THEN TRUE ELSE FALSE END AS IS_OPTIMIZATION_CONVERSION
+FROM {{ ref('int_facebook_conversions') }} f
+LEFT JOIN {{ ref('optimization_conversions') }} oc
+    ON f.ACCOUNT = oc.account
+    AND f.CONVERSIONNAME = oc.optimization_conversion
+    AND oc.platform = 'facebook'
 ORDER BY DATE DESC
      , CAMPAIGN ASC
      , ADSET ASC
