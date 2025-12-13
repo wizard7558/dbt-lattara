1392882e3720fa0fd6b2ac1147dbadbba480f106
riley@resorensonllc.com <riley@resorensonllc.com>
2025-12-13 20:28:37 +0000

    adding tubohome model

--- models/marts/clients/mongoose/turbohome_ads_performance.sql
+++ models/marts/clients/mongoose/turbohome_ads_performance.sql
@@ -6,15 +6,12 @@ SELECT
     'Google' AS platform,
     account,
     campaign,
-    ad_group,
+    adgroup,
     SUM(spend) AS spend,
     SUM(impressions) AS impressions,
     SUM(clicks) AS clicks,
     SUM(CASE WHEN conversion_action_name = 'Lead Score | 2' THEN conversions ELSE 0 END) AS lead_score_2,
-    SUM(CASE WHEN conversion_action_name = 'Lead Score | 3' THEN conversions ELSE 0 END) AS lead_score_3,
-    SUM(CASE WHEN conversion_action_name IN ('Lead Score | 2', 'Lead Score | 3') THEN conversions ELSE 0 END) AS total_optimization_conversions,
-    SUM(conversions) AS total_conversions,
-    SUM(conversion_value) AS conversion_value
+    SUM(CASE WHEN conversion_action_name = 'Lead Score | 3' THEN conversions ELSE 0 END) AS lead_score_3
 FROM {{ ref('google_ads_adgroup_performance') }}
 WHERE account = 'TurboHome'
 GROUP BY 1, 2, 3, 4, 5
